df5a91f84513483b09ec683058a2a45ca9ff7995
Bartu Çakır <bartuuu_lol@hotmail.com>
2024-02-22 01:21:11 +0300

    fix: projects responsive

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,7 +4,7 @@ import Loading from "./loading";
 
 const Page = ({}) => {
   return (
-    <div className="flex flex-col w-1/2 lg:mt-36 mt-12">
+    <div className="flex flex-col md:w-1/2 w-4/5 lg:mt-36 mt-12">
       <h1 className="text-xl text-sky-50 text-center mb-4">All Repositories</h1>
       <Suspense fallback={<Loading />}>
         <ProjectsContainer />
